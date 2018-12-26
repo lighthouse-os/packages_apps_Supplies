eea3eea8eb175ec9d3ed59bd4aa10928268cd716
Anushek Prasal <anushekprasal@gmail.com>
2018-12-27 02:54:40 +0530

    Supplies: Network traffic location [2/2]
    Signed-off-by: spezi77 <spezi7713@gmx.com>

--- src/com/lighthouse/settings/fragments/StatusBarSettings.java
+++ src/com/lighthouse/settings/fragments/StatusBarSettings.java
@@ -25,9 +25,6 @@ import android.text.TextUtils;
 import android.view.View;
 
 import com.android.settings.SettingsPreferenceFragment;
-import com.lighthouse.settings.preferences.CustomSeekBarPreference;
-import com.lighthouse.settings.preferences.SystemSettingSwitchPreference;
-import com.android.settings.Utils;
 import android.util.Log;
 
 import java.util.List;
@@ -43,9 +40,6 @@ import com.android.settingslib.search.SearchIndexable;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-    private CustomSeekBarPreference mThreshold;
-    private SystemSettingSwitchPreference mNetMonitor;
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -54,40 +48,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
         final ContentResolver resolver = getActivity().getContentResolver();
-
-        boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT) == 1;
-        mNetMonitor = (SystemSettingSwitchPreference) findPreference("network_traffic_state");
-        mNetMonitor.setChecked(isNetMonitorEnabled);
-        mNetMonitor.setOnPreferenceChangeListener(this);
-
-        int value = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
-        mThreshold = (CustomSeekBarPreference) findPreference("network_traffic_autohide_threshold");
-        mThreshold.setValue(value);
-        mThreshold.setOnPreferenceChangeListener(this);
-        mThreshold.setEnabled(isNetMonitorEnabled);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mNetMonitor) {
-            boolean value = (Boolean) objValue;
-            Settings.System.putIntForUser(getActivity().getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_STATE, value ? 1 : 0,
-                    UserHandle.USER_CURRENT);
-            mNetMonitor.setChecked(value);
-            mThreshold.setEnabled(value);
-            return true;
-        } else if (preference == mThreshold) {
-            int val = (Integer) objValue;
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
-                    UserHandle.USER_CURRENT);
-            return true;
-        }
-        return false;
-    }
 
     @Override
     public int getMetricsCategory() {
@@ -95,5 +59,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+    }
             new BaseSearchIndexProvider(R.xml.lighthouse_settings_statusbar);
 }
