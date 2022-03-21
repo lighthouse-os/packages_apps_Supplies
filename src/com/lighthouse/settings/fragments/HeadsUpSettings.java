9590d3b597cc8edc76c5c167cbfba70cb0d99759
CannedShroud <hypersync112@gmail.com>
2022-03-21 13:43:46 +0530

    Supplies: cleanup for 12.1

--- src/com/lighthouse/settings/fragments/HeadsUpSettings.java
+++ src/com/lighthouse/settings/fragments/HeadsUpSettings.java
@@ -43,53 +43,22 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-
-import com.lighthouse.settings.preference.AppListPreference;
-import com.lighthouse.settings.preference.PackageListAdapter;
-import com.lighthouse.settings.preference.PackageListAdapter.PackageItem;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class HeadsUpSettings extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceClickListener, Preference.OnPreferenceChangeListener {
-
-    private static final int DIALOG_BLACKLIST_APPS = 1;
-    private PackageListAdapter mPackageAdapter;
-    private PackageManager mPackageManager;
-    private PreferenceGroup mBlacklistPrefList;
-    private Preference mAddBlacklistPref;
-
-    private String mBlacklistPackageList;
-    private Map<String, Package> mBlacklistPackages;
+@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
+public class HeadsUpSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Get launch-able applications
         addPreferencesFromResource(R.xml.lighthouse_settings_heads_up);
-        mPackageManager = getPackageManager();
-        mPackageAdapter = new PackageListAdapter(getActivity());
-
-        mBlacklistPrefList = (PreferenceGroup) findPreference("blacklist_applications");
-        mBlacklistPrefList.setOrderingAsAdded(false);
-
-        mBlacklistPackages = new HashMap<String, Package>();
-
-        mAddBlacklistPref = findPreference("add_blacklist_packages");
-
-        mAddBlacklistPref.setOnPreferenceClickListener(this);
-
-        Resources systemUiResources;
-        try {
-            systemUiResources = getPackageManager().getResourcesForApplication("com.android.systemui");
-        } catch (Exception e) {
-            return;
-        }
-
     }
 
     @Override
@@ -98,212 +67,13 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        refreshCustomApplicationPrefs();
-    }
-
-    @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SUPPLIES;
     }
 
-    @Override
-    public int getDialogMetricsCategory(int dialogId) {
-        if (dialogId == DIALOG_BLACKLIST_APPS) {
-            return MetricsProto.MetricsEvent.SUPPLIES;
-        }
-        return 0;
-    }
-
     /**
-     * Utility classes and supporting methods
+     * For Search.
      */
-    @Override
-    public Dialog onCreateDialog(int id) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        final Dialog dialog;
-        final ListView list = new ListView(getActivity());
-        list.setAdapter(mPackageAdapter);
-
-        builder.setTitle(R.string.profile_choose_app);
-        builder.setView(list);
-        dialog = builder.create();
-
-        switch (id) {
-            case DIALOG_BLACKLIST_APPS:
-                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent,
-                            View view, int position, long id) {
-                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
-                        addCustomApplicationPref(info.packageName, mBlacklistPackages);
-                        dialog.cancel();
-                    }
-                });
-        }
-        return dialog;
-    }
-
-    /**
-     * Application class
-     */
-    private static class Package {
-        public String name;
-        /**
-         * Stores all the application values in one call
-         * @param name
-         */
-        public Package(String name) {
-            this.name = name;
-        }
-
-        public String toString() {
-            StringBuilder builder = new StringBuilder();
-            builder.append(name);
-            return builder.toString();
-        }
-
-        public static Package fromString(String value) {
-            if (TextUtils.isEmpty(value)) {
-                return null;
-            }
-
-            try {
-                Package item = new Package(value);
-                return item;
-            } catch (NumberFormatException e) {
-                return null;
-            }
-        }
-
-    };
-
-    private void refreshCustomApplicationPrefs() {
-        if (!parsePackageList()) {
-            return;
-        }
-
-        // Add the Application Preferences
-        if (mBlacklistPrefList != null) {
-            mBlacklistPrefList.removeAll();
-
-            for (Package pkg : mBlacklistPackages.values()) {
-                try {
-                    Preference pref = createPreferenceFromInfo(pkg);
-                    mBlacklistPrefList.addPreference(pref);
-                } catch (PackageManager.NameNotFoundException e) {
-                    // Do nothing
-                }
-            }
-        }
-
-        // Keep these at the top
-        mAddBlacklistPref.setOrder(0);
-        // Add 'add' options
-        mBlacklistPrefList.addPreference(mAddBlacklistPref);
-    }
-
-    @Override
-    public boolean onPreferenceClick(Preference preference) {
-        if (preference == mAddBlacklistPref) {
-            showDialog(DIALOG_BLACKLIST_APPS);
-        } else {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.dialog_delete_title)
-                    .setMessage(R.string.dialog_delete_message)
-                    .setIconAttribute(android.R.attr.alertDialogIcon)
-                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            if (preference == mBlacklistPrefList.findPreference(preference.getKey())) {
-                                 removeApplicationPref(preference.getKey(), mBlacklistPackages);
-                            }
-                        }
-                    })
-                    .setNegativeButton(android.R.string.cancel, null);
-
-        builder.show();
-        }
-        return true;
-    }
-
-     private void addCustomApplicationPref(String packageName, Map<String,Package> map) {
-        Package pkg = map.get(packageName);
-        if (pkg == null) {
-            pkg = new Package(packageName);
-            map.put(packageName, pkg);
-            savePackageList(false, map);
-            refreshCustomApplicationPrefs();
-        }
-    }
-
-    private Preference createPreferenceFromInfo(Package pkg)
-            throws PackageManager.NameNotFoundException {
-        PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
-                PackageManager.GET_META_DATA);
-        Preference pref =
-                new AppListPreference(getActivity());
-
-        pref.setKey(pkg.name);
-        pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
-        pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
-        pref.setPersistent(false);
-        pref.setOnPreferenceClickListener(this);
-        return pref;
-    }
-
-    private void removeApplicationPref(String packageName, Map<String,Package> map) {
-        if (map.remove(packageName) != null) {
-            savePackageList(false, map);
-            refreshCustomApplicationPrefs();
-        }
-    }
-
-    private boolean parsePackageList() {
-        boolean parsed = false;
-
-        final String blacklistString = Settings.System.getString(getContentResolver(),
-                Settings.System.HEADS_UP_BLACKLIST_VALUES);
-
-        if (!TextUtils.equals(mBlacklistPackageList, blacklistString)) {
-            mBlacklistPackageList = blacklistString;
-            mBlacklistPackages.clear();
-            parseAndAddToMap(blacklistString, mBlacklistPackages);
-            parsed = true;
-        }
-
-        return parsed;
-    }
-
-    private void parseAndAddToMap(String baseString, Map<String,Package> map) {
-        if (baseString == null) {
-            return;
-        }
-
-        final String[] array = TextUtils.split(baseString, "\\|");
-        for (String item : array) {
-            if (TextUtils.isEmpty(item)) {
-                continue;
-            }
-            Package pkg = Package.fromString(item);
-            map.put(pkg.name, pkg);
-        }
-    }
-
-
-    private void savePackageList(boolean preferencesUpdated, Map<String,Package> map) {
-        String setting = Settings.System.HEADS_UP_BLACKLIST_VALUES;
-
-        List<String> settings = new ArrayList<String>();
-        for (Package app : map.values()) {
-            settings.add(app.toString());
-        }
-        final String value = TextUtils.join("|", settings);
-        if (preferencesUpdated) {
-                mBlacklistPackageList = value;
-        }
-        Settings.System.putString(getContentResolver(),
-                setting, value);
-    }
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.lighthouse_settings_heads_up);
 }
